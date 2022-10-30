da59ed711be96def56be8f8e49d38d8eb2e17ae5
matis <matis.baguelin@gmail.com>
2022-10-30 16:57:00 +0100

    basics

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/App.css'
+
+function App() {
+  return (
+    <main>
+      {/* search bar */}
+      content
+      {/* search results */}
+    </main>
+  )
+}
+
+export default App
