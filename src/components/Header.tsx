85fc21dc9673ff807d9032178fa235892b9d67ea
matis <matis.baguelin@gmail.com>
2022-10-31 19:38:40 +0100

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="flex justify-center items-center py-2">
+      <img src="/beer.png" className="w-3 mr-4" alt="beer icon" />
+      <span className="text-xl font-semibold uppercase">Punk API</span>
+    </header>
+  )
+}
+
+export default Header
